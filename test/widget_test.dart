de1513d5bf28969b8f668389f9dc2c0f54f8118f
Fyfy Dev <101877327+streamdao@users.noreply.github.com>
2022-04-19 16:04:09 +0300

    Minor fix

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:fyfypay/main.dart';
+import 'package:streampay/main.dart';
 import 'package:streampay/state/store.dart';
 
 void main() {
